c771a1919b6d8c2e4426848c0f23be4f1f5671f3
Aadarsh <aadarsh-s@outlook.com>
2020-11-18 00:37:52 +0530

    Adding JOB card with images and extra content

--- src/components/Jobs.tsx
+++ src/components/Jobs.tsx
@@ -1,4 +1,4 @@
-import { Box, Spacer } from '@chakra-ui/react';
+import { Box, Flex, Image, Spacer, Text } from '@chakra-ui/react';
 import React from 'react';
 import { JOB } from '../interface/JOB';
 
@@ -9,19 +9,17 @@ interface JobsProps {
 export const Jobs: React.FC<JobsProps> = ({ job }) => {
   return (
     <React.Fragment>
-      <Box
-        w='48%'
-        h='35vh'
-        p='5'
-        m='7px'
-        // borderWidth='7px'
-        // borderColor='blue.50'
-        rounded='md'
-        overflow='hidden'
-        bg='gray.100'
-        color='black'
-      >
-        {job.title}
+      <Box w='48%' h='35vh' p='5' m='7px' rounded='md' overflow='hidden' bg='gray.100' color='black'>
+        <Text fontWeight='bold' fontSize='xl'>
+          {job.title}
+        </Text>
+        <Flex>
+          <p>{job.company}</p>
+          <p>{job.location}</p>
+          <p>{job.created_at}</p>
+          <p>{job.type}</p>
+        </Flex>
+        <Image boxSize='50%' objectFit='cover' src={job.company_logo} alt='Dan Abramov' />
       </Box>
       <Spacer />
     </React.Fragment>
