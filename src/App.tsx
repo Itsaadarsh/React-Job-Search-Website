63f159a3719a8b05c0114d674d64ac126c5ec2e1
Aadarsh <aadarsh-s@outlook.com>
2020-11-17 20:27:14 +0530

    Adding little charka UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { ChakraProvider, Input, Flex, Box, Text } from '@chakra-ui/react';
 
 const BASE_URL: string = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json';
 
@@ -32,28 +33,51 @@ function App() {
 
   useEffect(() => {
     async function asyncFetcher() {
-      const res = await axios.get(BASE_URL, { params: { description: role, location: location } });
-      console.log(res.data);
-
-      setjobs(res.data);
+      try {
+        const res = await axios.get(BASE_URL, { params: { description: role, location: location } });
+        console.log(res.data);
+        setjobs(res.data);
+        // console.log('Dummy');
+      } catch (err) {
+        console.log(err);
+      }
     }
     asyncFetcher();
   }, [role, location]);
 
   return (
-    <div>
-      <input type='text' placeholder='Role' value={role} onChange={e => setRole(e.target.value)} />
-      <input
-        type='text'
-        placeholder='Location'
-        value={location}
-        onChange={e => setLocation(e.target.value)}
-      />
-      <input type='checkbox' value='Full time' />
-      {jobs.map((job: JOB) => {
-        return <p key={job.id}>{job.title}</p>;
-      })}
-    </div>
+    <ChakraProvider>
+      <Box fontFamily='Righteous' boxShadow='dark-lg' p='6' mx='60' my='30' rounded='md' bg='blue.50'>
+        <Text fontSize='5xl' textAlign='center' pb='5' fontStyle='oblique' fontWeight='bold'>
+          Job Finder
+        </Text>
+        <Flex>
+          <Input
+            variant='flushed'
+            type='text'
+            placeholder='Role'
+            value={role}
+            onChange={e => setRole(e.target.value)}
+          />
+          <Input
+            variant='flushed'
+            type='text'
+            placeholder='Location'
+            value={location}
+            onChange={e => setLocation(e.target.value)}
+          />
+        </Flex>
+        <Flex justifyContent='space-between'>
+          {jobs.map((job: JOB) => {
+            return (
+              <Box bg='black' w='100%' p={4} color='white'>
+                {job.title}
+              </Box>
+            );
+          })}
+        </Flex>
+      </Box>
+    </ChakraProvider>
   );
 }
 
